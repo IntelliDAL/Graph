11997458b290dfef68f4d68e1a287bdd2966ebb1
IDAL <126667585+IntelliDAL@users.noreply.github.com>
2023-03-27 15:53:53 +0800

    Add files via upload

--- GraphSSL/GraphSSL_Main/frameworks/datasets/data_util.py
+++ GraphSSL/GraphSSL_Main/frameworks/datasets/data_util.py
@@ -1,131 +1,8 @@
 
-from collections import namedtuple, Counter
-import numpy as np
-
+from collections import Counter
 import torch
 import torch.nn.functional as F
-
-import dgl
-from dgl.data import (
-    load_data, 
-    TUDataset, 
-    CoraGraphDataset, 
-    CiteseerGraphDataset, 
-    PubmedGraphDataset
-)
-from ogb.nodeproppred import DglNodePropPredDataset
-from dgl.data.ppi import PPIDataset
-from dgl.dataloading import GraphDataLoader
-
-from sklearn.preprocessing import StandardScaler
-
-
-GRAPH_DICT = {
-    "cora": CoraGraphDataset,
-    "citeseer": CiteseerGraphDataset,
-    "pubmed": PubmedGraphDataset,
-    "ogbn-arxiv": DglNodePropPredDataset
-}
-
-
-def preprocess(graph):
-    feat = graph.ndata["feat"]
-    graph = dgl.to_bidirected(graph)
-    graph.ndata["feat"] = feat
-
-    graph = graph.remove_self_loop().add_self_loop()
-    graph.create_formats_()
-    return graph
-
-
-def scale_feats(x):
-    scaler = StandardScaler()
-    feats = x.numpy()
-    scaler.fit(feats)
-    feats = torch.from_numpy(scaler.transform(feats)).float()
-    return feats
-
-
-def load_dataset(dataset_name):
-    assert dataset_name in GRAPH_DICT, f"Unknow dataset: {dataset_name}."
-    if dataset_name.startswith("ogbn"):
-        dataset = GRAPH_DICT[dataset_name](dataset_name, raw_dir='./../GraphData')
-    else:
-        dataset = GRAPH_DICT[dataset_name](raw_dir='./../GraphData')
-
-    if dataset_name == "ogbn-arxiv":
-        graph, labels = dataset[0]
-        num_nodes = graph.num_nodes()
-
-        split_idx = dataset.get_idx_split()
-        train_idx, val_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
-        graph = preprocess(graph)
-
-        if not torch.is_tensor(train_idx):
-            train_idx = torch.as_tensor(train_idx)
-            val_idx = torch.as_tensor(val_idx)
-            test_idx = torch.as_tensor(test_idx)
-
-        feat = graph.ndata["feat"]
-        feat = scale_feats(feat)
-        graph.ndata["feat"] = feat
-
-        train_mask = torch.full((num_nodes,), False).index_fill_(0, train_idx, True)
-        val_mask = torch.full((num_nodes,), False).index_fill_(0, val_idx, True)
-        test_mask = torch.full((num_nodes,), False).index_fill_(0, test_idx, True)
-        graph.ndata["label"] = labels.view(-1)
-        graph.ndata["train_mask"], graph.ndata["val_mask"], graph.ndata["test_mask"] = train_mask, val_mask, test_mask
-    else:
-        graph = dataset[0]
-        graph = graph.remove_self_loop()
-        graph = graph.add_self_loop()
-    num_features = graph.ndata["feat"].shape[1]
-    num_classes = dataset.num_classes
-    return graph, (num_features, num_classes)
-
-
-def load_inductive_dataset(dataset_name):
-    if dataset_name == "ppi":
-        batch_size = 2
-        # define loss function
-        # create the dataset
-        train_dataset = PPIDataset(mode='train', raw_dir='./../GraphData')
-        valid_dataset = PPIDataset(mode='valid', raw_dir='./../GraphData')
-        test_dataset = PPIDataset(mode='test', raw_dir='./../GraphData')
-        train_dataloader = GraphDataLoader(train_dataset, batch_size=batch_size)
-        valid_dataloader = GraphDataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-        test_dataloader = GraphDataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-        eval_train_dataloader = GraphDataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-        g = train_dataset[0]
-        num_classes = train_dataset.num_labels
-        num_features = g.ndata['feat'].shape[1]
-    else:
-        _args = namedtuple("dt", "dataset")
-        dt = _args(dataset_name)
-        batch_size = 1
-        dataset = load_data(dt)
-        num_classes = dataset.num_classes
-
-        g = dataset[0]
-        num_features = g.ndata["feat"].shape[1]
-
-        train_mask = g.ndata['train_mask']
-        feat = g.ndata["feat"]
-        feat = scale_feats(feat)
-        g.ndata["feat"] = feat
-
-        g = g.remove_self_loop()
-        g = g.add_self_loop()
-
-        train_nid = np.nonzero(train_mask.data.numpy())[0].astype(np.int64)
-        train_g = dgl.node_subgraph(g, train_nid)
-        train_dataloader = [train_g]
-        valid_dataloader = [g]
-        test_dataloader = valid_dataloader
-        eval_train_dataloader = [train_g]
-        
-    return train_dataloader, valid_dataloader, test_dataloader, eval_train_dataloader, num_features, num_classes
-
+from dgl.data import TUDataset
 
 
 def load_graph_classification_dataset(dataset_name, deg4feat=False):
@@ -168,10 +45,10 @@ def load_graph_classification_dataset(dataset_name, deg4feat=False):
                 
                 feat = F.one_hot(degrees, num_classes=feature_dim).float()
 
-                g.ndata["attr"] = feat # 原有
-                # g.ndata["attr"] = feat[:, :271] # 后加 # 177
+                g.ndata["attr"] = feat # original code
+                # g.ndata["attr"] = feat[:, :271] # add for [Transfer learning cut features]
 
-            # feature_dim = 271   # feature_dim
+            # feature_dim = 271    # add for [feature_dim initial value]
 
     else:
         print("******** Use `attr` as node features ********")
